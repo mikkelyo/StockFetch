843918d5fcc97817b23d19c08d230c26725a1dab
Mikkel Ibsen <mikkel_i@hotmail.com>
2020-07-20 17:39:09 +0200

    added process function plus a few features

--- StockFetch.py
+++ StockFetch.py
@@ -14,59 +14,95 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 class Stocks:
-    ''' Class for fetching and processing stock data from NASDAQ and stuff '''
+    ''' Class for fetching and processing stock data from NASDAQ '''
     
     def __init__(self):
         self.symbol_list = []
+        self.processed_data = {}
         
     def symbol_init(self, input_list):
         self.symbol_list = input_list.split()
+        self.process(self.symbol_list)
+        return self.symbol_list
     
     def symbol_show(self):
         print(self.symbol_list)
-    
-#    def fetch(self, symbol=None, data_range='1d', data_interval='1m'):
-#        ''' Forklar hvilke værdier data range og data interval kan være '''
-#        res = requests.get('https://query1.finance.yahoo.com/v8/finance/chart/{symbol}?range={data_range}&interval={data_interval}'.format(**locals()))
-#        data = res.json()
-#        try:
-#            body = data['chart']['result'][0]
-#            dt = datetime.datetime
-#            dt = pd.Series(map(lambda x: arrow.get(x).to('CET').datetime.replace(tzinfo=None), body['timestamp']), name='Datetime')
-#            df = pd.DataFrame(body['indicators']['quote'][0], index=dt)
-#            dg = pd.DataFrame(body['timestamp'])
-#    
-#        except (UnboundLocalError, TypeError):
-#            print('Seems like you are calling a symbol that is not listed on YAHOO-finance.')
-#            return 
-#    
-#        output_data = df.loc[:, ('open', 'high', 'low', 'close', 'volume')].dropna() 
-#        
-#        
-#        #This loop is in place to delete any nonetype-data, does this need to be here?
-#        for number in range(len(list(output_data.keys()))):
-#        
-#            try:
-#                if type(output_data[(list(output_data.keys())[number])]) is type(None):
-#                    
-#                    print('***', (list(output_data.keys())[number]),'*** has no data. Entry deleted.')
-#                    del output_data[(list(output_data.keys())[number])]
-#                    
-#            except IndexError:
-#                pass
-#    
-#        return output_data 
-     
         
+    def get_dataframe(self):
+        df = self.processed_data
+        return df
     
+
+    def fetch(self, symbol=None, data_range='1d', data_interval='1m'):
+        ''' Forklar hvilke værdier data range og data interval kan være '''
+            
+        res = requests.get('https://query1.finance.yahoo.com/v8/finance/chart/{symbol}?range={data_range}&interval={data_interval}'.format(**locals()))
+        data = res.json()
+        try:
+            body = data['chart']['result'][0]
+            dt = datetime.datetime
+            dt = pd.Series(map(lambda x: arrow.get(x).to('CET').datetime.replace(tzinfo=None), body['timestamp']), name='Datetime')
+            df = pd.DataFrame(body['indicators']['quote'][0], index=dt)
+            dg = pd.DataFrame(body['timestamp'])
+    
+        except (UnboundLocalError, TypeError):
+            print('Seems like you are calling a symbol that is not listed on YAHOO-finance.')
+            return 
+    
+        output_data = df.loc[:, ('open', 'high', 'low', 'close', 'volume')].dropna() 
+        
+        
+        #This loop is in place to delete any nonetype-data, does this need to be here?
+        for number in range(len(list(output_data.keys()))):
+        
+            try:
+                if type(output_data[(list(output_data.keys())[number])]) is type(None):
+                    
+                    print('***', (list(output_data.keys())[number]),'*** has no data. Entry deleted.')
+                    del output_data[(list(output_data.keys())[number])]
+                    
+            except IndexError:
+                pass
+        return output_data 
+        
+    def process(self,data_range='1d', data_interval='1m'):
+        for symbol in self.symbol_list:
+            try:
+                self.processed_data[str(symbol)]=self.fetch(str(symbol),
+                                   data_range=data_range, data_interval=data_interval)
+            except KeyError:
+                print('One or more symbols are non-existant')
+                pass
     
     
 if __name__ == '__main__':
     
-#    stocks = Stocks().fetch('DIS','7d','1h')
-#    x= Stocks()
-#    x = Stocks().symbol_init('AAPL')
-    x=Stocks()
-    x.symbol_init('AAPL DIS TSLA AMD')
-    x.symbol_show()
+    x = Stocks()
+    x.symbol_init('DIS AAPL AMD GOOGL SYMC')
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    data = x.get_dataframe()
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
