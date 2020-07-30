df2c6531527c85501d133cc01331959f4db29a9b
Mikkel Ibsen <mikkel_i@hotmail.com>
2020-07-30 17:12:46 +0200

    added mortens thinking plus a lot of stuff

--- StockFetch.py
+++ StockFetch.py
@@ -52,7 +52,7 @@ class Stocks:
             dg = pd.DataFrame(body['timestamp'])
     
         except (UnboundLocalError, TypeError):
-            print('Seems like you are calling a symbol that is not listed on YAHOO-finance.')
+            print(symbol,'is not listed on YAHOO-finance')
             return 
     
         output_data = df.loc[:, ('open', 'high', 'low', 'close', 'volume')].dropna() 
@@ -79,7 +79,7 @@ class Stocks:
                 self.processed_data[str(symbol)]=self.fetch(str(symbol),
                                    data_range=data_range, data_interval=data_interval)
             except KeyError:
-                print('One or more symbols are non-existant')
+                print(symbol,'has no data')
                 pass
             
     def visualize_all(self):
@@ -94,30 +94,63 @@ class Stocks:
                 plt.grid(which='both')
                 plt.ylabel('Stock price [$]')
                 plt.xlabel('Date')
-                ticks = list(np.arange(0,len(self.processed_data[key]),len(self.processed_data[key])/15))
+                ticks = list(np.arange(0,len(self.processed_data[key])-1,len(self.processed_data[key])/15)) #if i dont subtract 1 from the length of arange, it can assume a length too long
                 ticks.append(len(self.processed_data[key])-1)
-                ticks = [int(round(i)) for i in ticks] #kinda silly way to make ticks include first and last tick - also is this precise enough?
+                ticks = [int(round(i)) for i in ticks]                          #kinda silly way to make ticks include first and last tick - also is this precise enough?
                 labels = self.processed_data[key].index[ticks]
                 plt.xticks( ticks=ticks , labels=labels , rotation = 'vertical')
+                plt.show()
         
         except AttributeError:
             print(key,'was not plotted due to data missing - check if symbol exists')
             
+    def morten(self):
+        '''Executes Morten's strategy'''
+        
+        
+        for key in self.processed_data:
+            try:
+                mean_tot = self.processed_data[key]['open'].mean()
+                mean_60 = self.processed_data[key]['open'][420:].mean() #past 60 days, find a better way to do this, perhaps use timestamps?
+                mean_10 = self.processed_data[key]['open'][70:].mean()  #past 10 days
+                max_tot = self.processed_data[key]['open'].max()    
+                price_now = self.processed_data[key]['open'][-1]
+                price_1day = self.processed_data[key]['open'][-8] #indices in the shape of hours - meaning 8 will go back to the previous business day
+                price_2days = self.processed_data[key]['open'][-16] #similar here will go back 2 days
     
+                
+                if price_now <= mean_tot and price_now <= mean_60 and price_now <= mean_10 and price_now <= max_tot and price_1day <= price_2days and price_now <= price_1day and price_now <= max_tot*0.7:
+                    print(key,'IS ON FIRE!!!')
+                    
+                    
+                else:
+#                    print(key,'does not fulfill Mortens criteria') #all this is used only for testing function
+                    pass
+            except TypeError:
+#                print(key,'is empty and was skipped')
+                pass    
     
+            
 if __name__ == '__main__':
     
     x = Stocks()
 #    x.symbol_init('DIS')
-    x.symbol_init('DIS AAPL AMD GOOGL SYMC')
-    x.process(data_range='1mo',data_interval='1h')
-    x.visualize_all()
+#    x.symbol_init('DIS AAPL AMD GOOGL SYMC KNDI SNSS TOPS NOVN INTC MSFT QCOM PLUG SPPI PEIX CMCSA JBLU CSCO PBCT SIRI WYNN MVIS FCEL NVDA AMZN AMAG MRNA BLDP GILD OPTT SGMO SCON SYPR')
+    x.symbol_init('FMC F FTNT FTV FBHS FOXA FOX BEN FCX GPS GRMN IT GD GE GIS GM GPC GILD GL GPN GS GWW HRB HAL HBI HIG HAS HCA PEAK HSIC HSY HES HPE HLT HFC HOLX HD HON HRL HST HWM HPQ HUM HBAN HII IEX IDXX INFO ITW ILMN INCY IR INTC ICE IBM IP IPG IFF INTU ISRG IVZ IPGP IQV IRM JKHY J JBHT SJM JNJ JCI JPM JNPR KSU K KEY KEYS KMB KIM KMI KLAC KSS KHC KR LB LHX LH LRCX LW LVS LEG LDOS LEN LLY LNC LIN LYV LKQ LMT L LOW LYB MTB MRO MPC MKTX MAR MMC MLM MAS MA MKC MXIM MCD MCK MDT MRK MET MTD MGM MCHP MU MSFT MAA MHK TAP MDLZ MNST MCO MS MOS MSI MSCI MYL NDAQ NOV NTAP NFLX NWL NEM NWSA NWS NEE NLSN NKE NI NBL NSC NTRS NOC NLOK NCLH NRG NUE NVDA NVR ORLY OXY ODFL OMC OKE ORCL OTIS PCAR PKG PH PAYX PAYC PYPL PNR PBCT PEP PKI PRGO PFE PM PSX PNW PXD PNC PPG PPL PFG PG PGR PLD PRU PEG PSA PHM PVH QRVO PWR QCOM DGX RL RJF RTX O REG REGN RF RSG RMD RHI ROK ROL ROP ROST RCL SPGI CRM SBAC SLB STX SEE SRE NOW SHW SPG SWKS SLG SNA SO LUV SWK SBUX STT STE SYK SIVB SYF SNPS SYY TMUS TROW TTWO TPR TGT TEL FTI TDY TFX TXN TXT TMO TIF TJX TSCO T TDG TRV TFC TWTR TYL TSN UDR ULTA USB UAA UA UNP UAL UNH UPS URI UHS UNM VFC VLO VAR VTR VRSN VRSK VZ VRTX VIAC V VNO VMC WRB WAB WMT WBA DIS WM WAT WEC WFC WELL WST WDC WU WRK WY WHR WMB WLTW WYNN XEL XRX XLNX XYL YUM ZBRA ZBH ZION ZTS MMM ABT ABBV ABMD ACN ATVI ADBE AMD AAP AES AFL A APD AKAM ALK ALB ARE ALXN ALGN ALLE LNT ALL GOOG MO AMZN AMCR AEE AAL AEP AXP AIG AMT AWK AMP ABC AME AMGN APH ADI ANSS ANTM AON AOS APA AIV AAPL AMAT APTV ADM ANET AJG AIZ T ATO ADSK ADP AZO AVB AVY BKR BLL BAC BK BAX BDX BBY BIO BIIB BLK BA BKNG BWA BXP BSX BMY AVGO BR B CHRW COG CDNS CPB COF CAH KMX CCL CARR CAT CBOE CBRE CDW CE CNC CNP CTL CERN CF SCHW CHTR CVX CMG CB CHD CI CINF CTAS CSCO C CFG CTXS CLX CME CMS KO CTSH CL CMCSA CMA CAG CXO COP ED STZ COO CPRT GLW CTVA COST COTY CCI CSX CMI CVS DHI DHR DRI DVA DE DAL XRAY DVN DXCM FANG DLR DFS DISCA DISCK DISH DG DLTR D DPZ DOV DOW DTE DUK DRE DD DXC ETFC EMN ETN EBAY ECL EIX EW EA EMR ETR EOG EFX EQIX EQR ESS EL EVRG ES RE EXC EXPE EXPD EXR XOM FFIV FB FAST FRT FDX FIS FITB FE FRC FISV FLT FLIR FLS ATVI ADBE amd  alxn algn goog GOOGL AMZN AMGN adi anss AAPL amat asml adsk adp bidu biib bmrn bkng avgo cdns cdw cern chtr ctas CSCO ctxs ctsh cmcsa cprt cost csx dxcm docu dltr EA EBAY exc expe FB fast fisv fox foxa gild idxx ilmn incy INTC intu isrg jd klac lrcx lbtya lbtyk lulu mar mxim meli mchp mu msft mrna mdlz MNST ntap ntes NFLX NVDA nxpi orly pcar payx pypl pep qcom regn rost sgen siri swks splk sbux snps tmus ttwo tsla txn khc tcom ulta vrsn vrsk vrtx wba wdc wday xel xlnx zm')
+    x.process(data_range='9mo',data_interval='1h')
     
-#    data = x.get_dataframe()
-
+    data = x.get_dataframe()
+    x.morten()
+#    x.visualize_all()
     
     
-    
+#%%
+    #homemade shit to test if there are duplicates XD
+#    test = 'FMC F FTNT FTV FBHS FOXA FOX BEN FCX GPS GRMN IT GD GE GIS GM GPC GILD GL GPN GS GWW HRB HAL HBI HIG HAS HCA PEAK HSIC HSY HES HPE HLT HFC HOLX HD HON HRL HST HWM HPQ HUM HBAN HII IEX IDXX INFO ITW ILMN INCY IR INTC ICE IBM IP IPG IFF INTU ISRG IVZ IPGP IQV IRM JKHY J JBHT SJM JNJ JCI JPM JNPR KSU K KEY KEYS KMB KIM KMI KLAC KSS KHC KR LB LHX LH LRCX LW LVS LEG LDOS LEN LLY LNC LIN LYV LKQ LMT L LOW LYB MTB MRO MPC MKTX MAR MMC MLM MAS MA MKC MXIM MCD MCK MDT MRK MET MTD MGM MCHP MU MSFT MAA MHK TAP MDLZ MNST MCO MS MOS MSI MSCI MYL NDAQ NOV NTAP NFLX NWL NEM NWSA NWS NEE NLSN NKE NI NBL NSC NTRS NOC NLOK NCLH NRG NUE NVDA NVR ORLY OXY ODFL OMC OKE ORCL OTIS PCAR PKG PH PAYX PAYC PYPL PNR PBCT PEP PKI PRGO PFE PM PSX PNW PXD PNC PPG PPL PFG PG PGR PLD PRU PEG PSA PHM PVH QRVO PWR QCOM DGX RL RJF RTX O REG REGN RF RSG RMD RHI ROK ROL ROP ROST RCL SPGI CRM SBAC SLB STX SEE SRE NOW SHW SPG SWKS SLG SNA SO LUV SWK SBUX STT STE SYK SIVB SYF SNPS SYY TMUS TROW TTWO TPR TGT TEL FTI TDY TFX TXN TXT TMO TIF TJX TSCO T TDG TRV TFC TWTR TYL TSN UDR ULTA USB UAA UA UNP UAL UNH UPS URI UHS UNM VFC VLO VAR VTR VRSN VRSK VZ VRTX VIAC V VNO VMC WRB WAB WMT WBA DIS WM WAT WEC WFC WELL WST WDC WU WRK WY WHR WMB WLTW WYNN XEL XRX XLNX XYL YUM ZBRA ZBH ZION ZTS MMM ABT ABBV ABMD ACN ATVI ADBE AMD AAP AES AFL A APD AKAM ALK ALB ARE ALXN ALGN ALLE LNT ALL GOOG MO AMZN AMCR AEE AAL AEP AXP AIG AMT AWK AMP ABC AME AMGN APH ADI ANSS ANTM AON AOS APA AIV AAPL AMAT APTV ADM ANET AJG AIZ T ATO ADSK ADP AZO AVB AVY BKR BLL BAC BK BAX BDX BBY BIO BIIB BLK BA BKNG BWA BXP BSX BMY AVGO BR B CHRW COG CDNS CPB COF CAH KMX CCL CARR CAT CBOE CBRE CDW CE CNC CNP CTL CERN CF SCHW CHTR CVX CMG CB CHD CI CINF CTAS CSCO C CFG CTXS CLX CME CMS KO CTSH CL CMCSA CMA CAG CXO COP ED STZ COO CPRT GLW CTVA COST COTY CCI CSX CMI CVS DHI DHR DRI DVA DE DAL XRAY DVN DXCM FANG DLR DFS DISCA DISCK DISH DG DLTR D DPZ DOV DOW DTE DUK DRE DD DXC ETFC EMN ETN EBAY ECL EIX EW EA EMR ETR EOG EFX EQIX EQR ESS EL EVRG ES RE EXC EXPE EXPD EXR XOM FFIV FB FAST FRT FDX FIS FITB FE FRC FISV FLT FLIR FLS ATVI ADBE amd  alxn algn goog GOOGL AMZN AMGN adi anss AAPL amat asml adsk adp bidu biib bmrn bkng avgo cdns cdw cern chtr ctas CSCO ctxs ctsh cmcsa cprt cost csx dxcm docu dltr EA EBAY exc expe FB fast fisv fox foxa gild idxx ilmn incy INTC intu isrg jd klac lrcx lbtya lbtyk lulu mar mxim meli mchp mu msft mrna mdlz MNST ntap ntes NFLX NVDA nxpi orly pcar payx pypl pep qcom regn rost sgen siri swks splk sbux snps tmus ttwo tsla txn khc tcom ulta vrsn vrsk vrtx wba wdc wday xel xlnx zm'
+#    test = test.split()
+#    
+#    test = list(dict.fromkeys(test))
+#    len(test) != len(set(test))
     
     
     
