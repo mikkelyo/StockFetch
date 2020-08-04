840a76d5e76471d40bb9827a5f1d267ca37c4ed6
Mikkel Ibsen <mikkel_i@hotmail.com>
2020-08-04 15:11:14 +0200

    added some randoms little things

--- StockFetch.py
+++ StockFetch.py
@@ -19,6 +19,7 @@ class Stocks:
     def __init__(self):
         self.symbol_list = []
         self.processed_data = {}
+        self.strat1_symbols = {}
         
     def symbol_init(self, input_list):
         '''Input desired symbols as a string and with a space separating them'''
@@ -32,11 +33,16 @@ class Stocks:
         
         print(self.symbol_list)
         
-    def get_dataframe(self):
-        '''Outputs the dataframe with all data to a variable'''
-        
-        df = self.processed_data
+    def get_dataframe(self,strat1=False):
+        '''Outputs the dataframe with all data to a variable
+        strat1 = True will output a dataframe with the stocks fulfiilling strat1
+        '''
+        if strat1 == True:
+            df = self.strat1_symbols
+        else:    
+            df = self.processed_data
         return df
+    
     
 
     def fetch(self, symbol=None, data_range='1d', data_interval='1m'):
@@ -83,7 +89,7 @@ class Stocks:
                 pass
             
     def visualize_all(self):
-        '''Plots all current symbols - individual plotter to be added'''
+        '''Plots all current symbols'''
         
         try:
             for key in self.processed_data:
@@ -103,8 +109,8 @@ class Stocks:
         
         except AttributeError:
             print(key,'was not plotted due to data missing - check if symbol exists')
-            
-    def morten(self):
+         
+    def strat1(self):
         '''Executes Morten's strategy'''
         
         
@@ -120,23 +126,24 @@ class Stocks:
     
                 
                 if price_now <= mean_tot and price_now <= mean_60 and price_now <= mean_10 and price_now <= max_tot and price_1day <= price_2days and price_now <= price_1day and price_now <= max_tot*0.7:
-                    print(key,'IS ON FIRE!!!')
-                    
+                    self.strat1_symbols[key] = self.processed_data[key]
+                
                     
                 else:
 #                    print(key,'does not fulfill Mortens criteria') #all this is used only for testing function
                     pass
+                
+                    
             except TypeError:
 #                print(key,'is empty and was skipped')
                 pass    
-    
+        print(list(self.strat1_symbols.keys()),'fulfill strategy 1s criteria')
             
     def visualize(self,symbol):
         '''Plots a symbol'''
         
         try:
             
-#            data = self.processed_data[symbol]    
             plt.figure(symbol,figsize=[12,3])
             plt.plot(self.processed_data[symbol].reset_index().Datetime.index,self.processed_data[symbol]['open'], alpha = 0.9, linewidth = 2 , label=symbol)
             plt.title(symbol)
@@ -156,13 +163,14 @@ class Stocks:
 if __name__ == '__main__':
     
     x = Stocks()
-#    x.symbol_init('DIS')
-    x.symbol_init('DIS AAPL AMD GOOGL SYMC KNDI SNSS TOPS NOVN INTC MSFT QCOM PLUG SPPI PEIX CMCSA JBLU CSCO PBCT SIRI WYNN MVIS FCEL NVDA AMZN AMAG MRNA BLDP GILD OPTT SGMO SCON SYPR')
-#    x.symbol_init('FMC F FTNT FTV FBHS FOXA FOX BEN FCX GPS GRMN IT GD GE GIS GM GPC GILD GL GPN GS GWW HRB HAL HBI HIG HAS HCA PEAK HSIC HSY HES HPE HLT HFC HOLX HD HON HRL HST HWM HPQ HUM HBAN HII IEX IDXX INFO ITW ILMN INCY IR INTC ICE IBM IP IPG IFF INTU ISRG IVZ IPGP IQV IRM JKHY J JBHT SJM JNJ JCI JPM JNPR KSU K KEY KEYS KMB KIM KMI KLAC KSS KHC KR LB LHX LH LRCX LW LVS LEG LDOS LEN LLY LNC LIN LYV LKQ LMT L LOW LYB MTB MRO MPC MKTX MAR MMC MLM MAS MA MKC MXIM MCD MCK MDT MRK MET MTD MGM MCHP MU MSFT MAA MHK TAP MDLZ MNST MCO MS MOS MSI MSCI MYL NDAQ NOV NTAP NFLX NWL NEM NWSA NWS NEE NLSN NKE NI NBL NSC NTRS NOC NLOK NCLH NRG NUE NVDA NVR ORLY OXY ODFL OMC OKE ORCL OTIS PCAR PKG PH PAYX PAYC PYPL PNR PBCT PEP PKI PRGO PFE PM PSX PNW PXD PNC PPG PPL PFG PG PGR PLD PRU PEG PSA PHM PVH QRVO PWR QCOM DGX RL RJF RTX O REG REGN RF RSG RMD RHI ROK ROL ROP ROST RCL SPGI CRM SBAC SLB STX SEE SRE NOW SHW SPG SWKS SLG SNA SO LUV SWK SBUX STT STE SYK SIVB SYF SNPS SYY TMUS TROW TTWO TPR TGT TEL FTI TDY TFX TXN TXT TMO TIF TJX TSCO T TDG TRV TFC TWTR TYL TSN UDR ULTA USB UAA UA UNP UAL UNH UPS URI UHS UNM VFC VLO VAR VTR VRSN VRSK VZ VRTX VIAC V VNO VMC WRB WAB WMT WBA DIS WM WAT WEC WFC WELL WST WDC WU WRK WY WHR WMB WLTW WYNN XEL XRX XLNX XYL YUM ZBRA ZBH ZION ZTS MMM ABT ABBV ABMD ACN ATVI ADBE AMD AAP AES AFL A APD AKAM ALK ALB ARE ALXN ALGN ALLE LNT ALL GOOG MO AMZN AMCR AEE AAL AEP AXP AIG AMT AWK AMP ABC AME AMGN APH ADI ANSS ANTM AON AOS APA AIV AAPL AMAT APTV ADM ANET AJG AIZ T ATO ADSK ADP AZO AVB AVY BKR BLL BAC BK BAX BDX BBY BIO BIIB BLK BA BKNG BWA BXP BSX BMY AVGO BR B CHRW COG CDNS CPB COF CAH KMX CCL CARR CAT CBOE CBRE CDW CE CNC CNP CTL CERN CF SCHW CHTR CVX CMG CB CHD CI CINF CTAS CSCO C CFG CTXS CLX CME CMS KO CTSH CL CMCSA CMA CAG CXO COP ED STZ COO CPRT GLW CTVA COST COTY CCI CSX CMI CVS DHI DHR DRI DVA DE DAL XRAY DVN DXCM FANG DLR DFS DISCA DISCK DISH DG DLTR D DPZ DOV DOW DTE DUK DRE DD DXC ETFC EMN ETN EBAY ECL EIX EW EA EMR ETR EOG EFX EQIX EQR ESS EL EVRG ES RE EXC EXPE EXPD EXR XOM FFIV FB FAST FRT FDX FIS FITB FE FRC FISV FLT FLIR FLS ATVI ADBE amd  alxn algn goog GOOGL AMZN AMGN adi anss AAPL amat asml adsk adp bidu biib bmrn bkng avgo cdns cdw cern chtr ctas CSCO ctxs ctsh cmcsa cprt cost csx dxcm docu dltr EA EBAY exc expe FB fast fisv fox foxa gild idxx ilmn incy INTC intu isrg jd klac lrcx lbtya lbtyk lulu mar mxim meli mchp mu msft mrna mdlz MNST ntap ntes NFLX NVDA nxpi orly pcar payx pypl pep qcom regn rost sgen siri swks splk sbux snps tmus ttwo tsla txn khc tcom ulta vrsn vrsk vrtx wba wdc wday xel xlnx zm')
+    x.symbol_init('DIS AAPL maersk-b.co INTC L RL CCL')
+#    x.symbol_init('DIS AAPL AMD GOOGL SYMC KNDI SNSS TOPS NOVN INTC MSFT QCOM PLUG SPPI PEIX CMCSA JBLU CSCO PBCT SIRI WYNN MVIS FCEL NVDA AMZN AMAG MRNA BLDP GILD OPTT SGMO SCON SYPR')
+#    x.symbol_init('FMC F FTNT FTV FBHS FOXA FOX BEN FCX GPS GRMN IT GD GE GIS GM GPC GILD GL GPN GS GWW HRB HAL HBI HIG HAS HCA PEAK HSIC HSY HES HPE HLT HFC HOLX HD HON HRL HST HWM HPQ HUM HBAN HII IEX IDXX INFO ITW ILMN INCY IR INTC ICE IBM IP IPG IFF INTU ISRG IVZ IPGP IQV IRM JKHY J JBHT SJM JNJ JCI JPM JNPR KSU K KEY KEYS KMB KIM KMI KLAC KSS KHC KR LB LHX LH LRCX LW LVS LEG LDOS LEN LLY LNC LIN LYV LKQ LMT L LOW LYB MTB MRO MPC MKTX MAR MMC MLM MAS MA MKC MXIM MCD MCK MDT MRK MET MTD MGM MCHP MU MSFT MAA MHK TAP MDLZ MNST MCO MS MOS MSI MSCI MYL NDAQ NOV NTAP NFLX NWL NEM NWSA NWS NEE NLSN NKE NI NBL NSC NTRS NOC NLOK NCLH NRG NUE NVDA NVR ORLY OXY ODFL OMC OKE ORCL OTIS PCAR PKG PH PAYX PAYC PYPL PNR PBCT PEP PKI PRGO PFE PM PSX PNW PXD PNC PPG PPL PFG PG PGR PLD PRU PEG PSA PHM PVH QRVO PWR QCOM DGX RL RJF RTX O REG REGN RF RSG RMD RHI ROK ROL ROP ROST RCL SPGI CRM SBAC SLB STX SEE SRE NOW SHW SPG SWKS SLG SNA SO LUV SWK SBUX STT STE SYK SIVB SYF SNPS SYY TMUS TROW TTWO TPR TGT TEL FTI TDY TFX TXN TXT TMO TIF TJX TSCO T TDG TRV TFC TWTR TYL TSN UDR ULTA USB UAA UA UNP UAL UNH UPS URI UHS UNM VFC VLO VAR VTR VRSN VRSK VZ VRTX VIAC V VNO VMC WRB WAB WMT WBA DIS WM WAT WEC WFC WELL WST WDC WU WRK WY WHR WMB WLTW WYNN XEL XRX XLNX XYL YUM ZBRA ZBH ZION ZTS MMM ABT ABBV ABMD ACN ATVI ADBE AMD AAP AES AFL A APD AKAM ALK ALB ARE ALXN ALGN ALLE LNT ALL GOOG MO AMZN AMCR AEE AAL AEP AXP AIG AMT AWK AMP ABC AME AMGN APH ADI ANSS ANTM AON AOS APA AIV AAPL AMAT APTV ADM ANET AJG AIZ T ATO ADSK ADP AZO AVB AVY BKR BLL BAC BK BAX BDX BBY BIO BIIB BLK BA BKNG BWA BXP BSX BMY AVGO BR B CHRW COG CDNS CPB COF CAH KMX CCL CARR CAT CBOE CBRE CDW CE CNC CNP CTL CERN CF SCHW CHTR CVX CMG CB CHD CI CINF CTAS CSCO C CFG CTXS CLX CME CMS KO CTSH CL CMCSA CMA CAG CXO COP ED STZ COO CPRT GLW CTVA COST COTY CCI CSX CMI CVS DHI DHR DRI DVA DE DAL XRAY DVN DXCM FANG DLR DFS DISCA DISCK DISH DG DLTR D DPZ DOV DOW DTE DUK DRE DD DXC ETFC EMN ETN EBAY ECL EIX EW EA EMR ETR EOG EFX EQIX EQR ESS EL EVRG ES RE EXC EXPE EXPD EXR XOM FFIV FB FAST FRT FDX FIS FITB FE FRC FISV FLT FLIR FLS ATVI ADBE amd  alxn algn goog GOOGL AMZN AMGN adi anss AAPL amat asml adsk adp bidu biib bmrn bkng avgo cdns cdw cern chtr ctas CSCO ctxs ctsh cmcsa cprt cost csx dxcm docu dltr EA EBAY exc expe FB fast fisv fox foxa gild idxx ilmn incy INTC intu isrg jd klac lrcx lbtya lbtyk lulu mar mxim meli mchp mu msft mrna mdlz MNST ntap ntes NFLX NVDA nxpi orly pcar payx pypl pep qcom regn rost sgen siri swks splk sbux snps tmus ttwo tsla txn khc tcom ulta vrsn vrsk vrtx wba wdc wday xel xlnx zm AC.PA F7TB.MU MAERSK-B.CO')
     x.process(data_range='9mo',data_interval='1h')
-    x.visualize(symbol='DIS')
-#    data = x.get_dataframe()
-#    x.morten()
+#    x.visualize(symbol='MAERSK-B.CO')
+    x.strat1()
+    data = x.get_dataframe(strat1=True)
+
 #    x.visualize_all()
     
     
@@ -173,7 +181,6 @@ if __name__ == '__main__':
 #    
 #    test = list(dict.fromkeys(test))
 #    len(test) != len(set(test))
-    
     
     
     
